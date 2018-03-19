1a5f4596444217525f896fa47dc33d3d0ef424ec
Patrick Medaugh <patrickmedaugh@gmail.com>
2018-03-19 16:52:13 -0600

    Changes some types for vineyard-minotaur rework

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,4 @@
-import {BigNumber} from 'bignumber.js'
+import { BigNumber } from 'bignumber.js'
 
 export type Id = string
 
@@ -18,54 +18,15 @@ export interface Address extends NewAddress {
   id: Id
 }
 
-export interface AddressRecord extends Address {
-  address: string
-  currency: number
-}
-
-export interface BaseBlock {
+export interface NewBlock {
   hash: string
   index: number
   currency: number
   timeMined: Date
 }
 
-export type NewBlock = BaseBlock
-
-export interface BlockInfo extends BaseBlock {
+export interface Block extends NewBlock {
   id: Id
-}
-
-export type Resolve<T> = (value: PromiseLike<T>|T|undefined) => void
-
-export enum TransactionStatus {
-  pending = 0,
-  accepted = 1,
-  rejected = 2,
-}
-
-export interface BaseTransaction {
-  txid: string
-  amount: BigNumber
-  timeReceived: Date
-  blockIndex: number,
-  status: TransactionStatus
-}
-
-export interface SingleTransactionProperties {
-  to: string
-  from: string
-}
-
-export type NewSingleTransaction = BaseTransaction & SingleTransactionProperties
-
-export interface SingleTransaction extends NewSingleTransaction {
-  id: Id
-  currency: number
-}
-
-export interface ExternalSingleTransaction extends BaseTransaction, SingleTransactionProperties {
-  confirmations: number
 }
 
 export interface ExternalBlock {
@@ -78,17 +39,43 @@ export interface FullBlock<ExternalTransaction> extends ExternalBlock {
   transactions: ExternalTransaction[]
 }
 
-export interface ReadClient<ExternalTransaction extends BaseTransaction> {
+export type Resolve<T> = (value: PromiseLike<T>|T|undefined) => void
 
+export enum TransactionStatus {
+  pending = 0,
+  accepted = 1,
+  rejected = 2,
+}
+
+export interface NewTransaction {
+  txid: string
+  amount: BigNumber
+  timeReceived: Date
+  block: number
+  status: TransactionStatus
+  to: string
+  from: string
+  currency: number
+}
+
+export interface Transaction extends NewTransaction {
+  id: Id
+}
+
+export interface ExternalTransaction extends NewTransaction {
+  confirmations: number
+}
+
+export interface ReadClient<ExternalTransaction> {
   getBlockIndex(): Promise<number>
 
-  getLastBlock(): Promise<BaseBlock>
+  getLastBlock(): Promise<Block>
 
   getTransactionStatus(txid: string): Promise<TransactionStatus>
 
-  getNextBlockInfo(block: BaseBlock | undefined): Promise<BaseBlock | undefined>
-  
-  getFullBlock(block: BaseBlock): Promise<FullBlock<ExternalTransaction> | undefined>
+  getNextBlockInfo(block: Block | undefined): Promise<Block | undefined>
+
+  getFullBlock(block: Block): Promise<FullBlock<ExternalTransaction> | undefined>
 }
 
 export interface BitcoinTransactionInfo {
@@ -96,10 +83,10 @@ export interface BitcoinTransactionInfo {
   used: boolean
 }
 
-export type BitcoinTransaction = BaseTransaction & BitcoinTransactionInfo
+export type BitcoinTransaction = NewTransaction & BitcoinTransactionInfo
 
-export interface BitcoinReadClient<Transaction extends BitcoinTransaction, T extends BaseTransaction> extends ReadClient<T> {
-  getFullBitcoinBlock(block: BlockInfo): Promise<FullBlock<Transaction> | undefined>
+export interface BitcoinReadClient<Transaction extends BitcoinTransaction, T extends NewTransaction> extends ReadClient<T> {
+  getFullBitcoinBlock(block: Block): Promise<FullBlock<Transaction> | undefined>
 }
 
 export interface WriteClient {
